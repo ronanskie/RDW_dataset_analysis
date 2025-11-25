4dbb9a9380b0a6224ac2ce6e004a54e82864ec80
Ronan <ronan@RonanDesktop.home>
2025-11-25 17:35:49 +0100

    updated custom legend

--- output_generator.py
+++ output_generator.py
@@ -56,7 +56,6 @@ class Output_Generator:
             line_opacity=0.2,
             nan_fill_color="white",
             bins=[0, 25000, 200000, 400000, 800000, 1600000, 2400000, max_count],
-            legend_name="Number of Vehicles per Country",
             highlight=True
         ).add_to(map)
 
@@ -66,4 +65,71 @@ class Output_Generator:
             style_function=lambda x: {"fillOpacity": 0, "weight": 0}
         ).add_to(map)
 
+        # Remove default legend
+        for key in list(map._children):
+            if key.startswith('color_map_'):
+                del map._children[key]
+
+        # Custom HTML for legend
+        legend = """
+            <div style="
+                position: fixed; 
+                bottom: 50px; 
+                left: 50px; 
+                width: 150px; 
+                background-color: white;
+                border: 2px solid #444;
+                padding: 10px;
+                z-index: 9999;
+                font-size: 12px;
+                display: flex;
+                flex-direction: column;
+            ">
+                <div style="margin-bottom:8px; font-weight:bold; text-align:center; width:100%;">
+                    <b>Vehicles per Country</b><br>
+                </div>
+
+                <div style="display:flex; flex-direction: column; width:100%;">
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#f7fcf5;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>0 - 25k</span>
+                    </div>
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#e5f5e0;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>25k - 200k</span>
+                    </div>
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#c7e9c0;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>200k - 400k</span>
+                    </div>
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#a1d99b;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>400k - 800k</span>
+                    </div>
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#74c476;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>800k - 1.6M</span>
+                    </div>
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#31a354;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>1.6M - 2.4M</span>
+                    </div>
+
+                    <div style="display:flex; align-items:center; margin-bottom:0px;">
+                        <div style="background:#006d2c;width:20px;height:20px;margin-right:8px;"></div>
+                        <span>2.4M - Max</span>
+                    </div>
+                </div>
+            </div>
+        """
+
+        map.get_root().html.add_child(folium.Element(legend))
+
+        # Save the map to an HTML file
         map.save("output_map.html")

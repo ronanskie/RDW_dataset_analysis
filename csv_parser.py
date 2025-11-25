b8da33ad9d66c6af1a73728637459821d618ff2e
Ronan <ronan@RonanDesktop.home>
2025-11-25 12:58:42 +0100

    data is now parsed in chunks to reduce memory usage, also added more comments

--- csv_parser.py
+++ csv_parser.py
@@ -10,21 +10,45 @@ import warnings
 class CSV_Parser:
 
     '''
-    csv_to_array(file_path): parse a csv file and return an array
-    file_path: the path to the csv file, should be placed in the dataset dir
+    csv_to_chunks(file_path, chunk_size=50000): parses a csv file and yields chunks of data
+    file_path: the path to the csv file.
+    return: a generator yielding numpy arrays containing the csv data chunks
+    '''
+    def csv_to_chunks(self, file_path, chunk_size=50000):
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore', category=UserWarning)
+            
+            csv_chunks = pd.read_csv(
+                file_path, 
+                delimiter=',', 
+                chunksize=chunk_size, 
+                header=None,
+                on_bad_lines='skip',
+                dtype=str
+            )
+            
+            # Generator that returns one chunk at a time as a numpy array
+            for chunk in csv_chunks:
+                yield chunk.to_numpy()
+
+    '''
+    csv_to_array(file_path): parses a csv file into a numpy array
+    file_path: the path to the csv file.
     return: a numpy array containing the csv data
     '''
     def csv_to_array(self, file_path):
         with warnings.catch_warnings():
             warnings.filterwarnings('ignore', category=UserWarning)
-
-            data = genfromtxt(
-                file_path,
-                delimiter=',',
-                dtype=None, 
-                invalid_raise=False
+            
+            df = pd.read_csv(
+                file_path, 
+                delimiter=',', 
+                header=None,
+                on_bad_lines='skip',
+                dtype=str
             )
-        return data
+
+            return df.to_numpy()
     
     '''
     filter_array(data): Filter strings in numpy array on quotation marks
@@ -32,5 +56,7 @@ class CSV_Parser:
     return: A numpy array with filtered strings (without quotation marks)
     '''
     def filter_array(self, data):
+        data = data.astype(str)
         filtered_data = np.char.replace(data, '"', '')
+
         return filtered_data
--- main.py
+++ main.py
@@ -2,41 +2,78 @@
 # Generate a plot of data from a csv file containing RDW vehicle data
 
 # Import libraries
-import sys
 import os
+import numpy as np
 from csv_parser import CSV_Parser
 from output_generator import Output_Generator
 from data_profiler import Data_Profiler
 
 # Constant variables
 BASE_DIR = os.path.dirname(__file__)
-DATASET = "RDW_dataset_sample.csv"
+DATASET = "RDW_dataset_col3.csv"
 CATEGORIES = "Car_Manufacturers.csv"
 DATASET_PATH = os.path.join(BASE_DIR, "dataset", DATASET)
 CATEGORIES_PATH = os.path.join(BASE_DIR, "dataset", CATEGORIES)
 
-def get_results(data, cat_data):
+'''
+aggregate_counts(global_counts, chunk_counts): aggregates counts from chunk into global counts
+global_counts: dictionary containing the global counts
+chunk_counts: 2D array containing the counts from the current chunk
+return: a generator yielding numpy arrays containing the csv data chunks
+'''
+def aggregate_counts(global_counts, chunk_counts):
+
+    for country, count in chunk_counts:
+        count = int(count) 
+        global_counts[country] = global_counts.get(country, 0) + count
+
+    return global_counts
+
+'''
+get_results(data_generator, cat_data): collects results from data generator and category data
+data_generator: generator yielding data chunks from the csv file
+cat_data: the category data to merge with the amount of occurrences
+return: a 2D array containing the final results
+'''
+def get_results(data_generator, cat_data):
     profiler = Data_Profiler()
+    parser = CSV_Parser()
+
+    global_country_counts = {}
     
-    counted_brands = profiler.count_occurrences(data)
-    counted_countries = profiler.merge_categories(counted_brands, cat_data)
+    for data in data_generator:
+        brand_column = data[:, 0] 
+        filtered_brands = parser.filter_array(brand_column)
+        counted_brands = profiler.count_occurrences(filtered_brands) 
+        counted_countries = profiler.merge_categories(counted_brands, cat_data)
+        global_country_counts = aggregate_counts(global_country_counts, counted_countries)
 
-    print(counted_countries)
+    final_array = np.array(list(global_country_counts.items()))
 
+    return final_array
+
+'''
+get_data(): gets the data from the csv datafile
+return: a generator yielding chunks of the data and the category data as a numpy array
+'''
 def get_data():
     parser = CSV_Parser()
 
-    # Load data from csv files
-    data = parser.csv_to_array(DATASET_PATH)
     cat_data = parser.csv_to_array(CATEGORIES_PATH)
+    cat_data = parser.filter_array(cat_data) 
 
-    # Filter array to remove quotation marks
-    data = parser.filter_array(data)
-    
-    return data, cat_data
+    data_generator = parser.csv_to_chunks(DATASET_PATH)
 
+    return data_generator, cat_data
+
+'''
+main(): Calls the functions needed to parse the data and generate the output
+return: Program exits after executing
+'''
 def main():
-    data, cat_data = get_data()
-    get_results(data, cat_data)
+    data_generator, cat_data = get_data() 
+    result = get_results(data_generator, cat_data)
+
+    print(result)
 
 main()
